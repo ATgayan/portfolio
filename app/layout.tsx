f0198fdaf1fca8f62d9d12465b0f36569f9b619f
AThusithaGayan <thusithag@aplicy.com>
2025-12-30 17:18:42 +0530

    fix google font err

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type React from "react"
 import "./globals.css"
-import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 
 import { Analytics } from "@vercel/analytics/next"
 
-const inter = Inter({ subsets: ["latin"] })
+
 
 export const metadata = {
   title: "Thusitha Gayan - Portfolio",
@@ -20,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body >
         <Analytics/>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
